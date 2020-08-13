1fe1ae5b8c2eaeae3f3be318be34b6b7eeba9db1
Syeda <55826878+syedaCodes@users.noreply.github.com>
2020-08-13 08:46:03 +0530

    portfolio stage #1

---
+++ app.js
@@ -0,0 +1,4 @@
+gsap.from('.header', { duration: 1, y: '-100%', ease: 'power4' });
+gsap.from('.aboutBtn', { opacity: 0, duration: 1, y: '100%', ease: 'power4' });
+gsap.from('.brand', { opacity: 0, duration: 1, y: '100', ease: 'power4' });
+
