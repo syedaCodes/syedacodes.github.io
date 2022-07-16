383e26b69bf7ac369ae04231d8994a5a9df1f797
Syeda <55826878+syedaCodes@users.noreply.github.com>
2022-07-16 22:05:36 +0530

    remove console.log

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const footer = document.getElementById('footer');
 
 let date = new Date().getFullYear();
-console.log(date)
 const html = `<p class="copyright">Made with ❤️ &copy Copyright ${date}</p>`;
 footer.insertAdjacentHTML('beforeend', html);
