4c1779b86b6c13688c6be7e043ed04621477bc8c
Syeda <55826878+syedaCodes@users.noreply.github.com>
2022-07-16 21:54:02 +0530

    Delete old app.js

--- app.js
+++
@@ -1,4 +0,0 @@
-gsap.from('.header', { duration: 1, y: '-100%', ease: 'power4' });
-gsap.from('.aboutBtn', { opacity: 0, duration: 1, y: '100%', ease: 'power4' });
-gsap.from('.brand', { opacity: 0, duration: 1, y: '100', ease: 'power4' });
-
